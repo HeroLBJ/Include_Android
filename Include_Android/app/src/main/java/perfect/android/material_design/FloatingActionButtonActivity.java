34fbf8d70c2bccf49ea3ae0223f586a24d995258
HeroLBJ <617416791@qq.com>
2019-03-28 22:02:00 +0800

    FloatingActionButton

---
+++ Include_Android/app/src/main/java/perfect/android/material_design/FloatingActionButtonActivity.java
@@ -0,0 +1,19 @@
+package perfect.android.material_design;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import perfect.android.R;
+
+/**
+ * @author Flower 2019/3/28
+ */
+public class FloatingActionButtonActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_design_floating_action_button);
+    }
+}
